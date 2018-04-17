31ae55fd563be5e6630c99686799fd84771e34df
jcbalunis <jbalunis@mail.csuchico.edu>
2018-04-16 17:28:13 -0700

    Yeah I am done With this. Time to ride home in the rain

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+
+void rearrangeColor (int&, int&, int&);
 int main()
 {
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -10,7 +12,7 @@ int main()
   //...END OF "DO NOT CHANGE" AREA
 
 
-
+  rearrangeColor( red, green, blue);
 
 
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -18,4 +20,43 @@ int main()
   cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
   return 0;
   //...END OF "DO NOT CHANGE" AREA
+}
+
+void rearrangeColor (int& Red, int& Green, int& Blue)
+{
+       int tempR, tempG, tempB;
+       
+       tempR = Red;
+       tempG = Green;
+       tempB = Blue;
+
+       if( Green > Red )
+       {
+       Red = tempG;
+       }
+       if( Blue > Red) 
+       {
+       Red = tempB;
+       }
+       if(( Green < Blue) &&( Red != tempB))
+       {
+       Green = tempB;
+       }
+       if(( Green < Red) && (Red != tempR))
+       {
+       Green = tempR;
+       }
+       if(Blue > tempR)
+       { 
+       Blue = tempR;
+       }
+       if( Blue > tempG)
+       {
+       Blue = tempG;
+       }
+   
+
+       
+
+
 }
