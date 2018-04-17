505938ff4718fc197e5aac8b6b9d24d0ff6f5912
jcbalunis <jbalunis@mail.csuchico.edu>
2018-04-16 17:36:40 -0700

    Yeah I am done With this. Time to ride home in the rain 2

--- main.cpp
+++ main.cpp
@@ -5,58 +5,58 @@ using namespace std;
 void rearrangeColor (int&, int&, int&);
 int main()
 {
-  //DO NOT CHANGE WITHIN THIS AREA...
-  int red, blue, green;
-  cout<<"Enter Red, Green, and Blue values: ";
-  cin>>red>>green>>blue;
-  //...END OF "DO NOT CHANGE" AREA
+        //DO NOT CHANGE WITHIN THIS AREA...
+        int red, blue, green;
+        cout<<"Enter Red, Green, and Blue values: ";
+        cin>>red>>green>>blue;
+        //...END OF "DO NOT CHANGE" AREA
 
 
-  rearrangeColor( red, green, blue);
+        rearrangeColor( red, green, blue);
 
 
-  //DO NOT CHANGE WITHIN THIS AREA...
-  cout<<"Rearranged....\n";
-  cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
-  return 0;
-  //...END OF "DO NOT CHANGE" AREA
+        //DO NOT CHANGE WITHIN THIS AREA...
+        cout<<"Rearranged....\n";
+        cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
+        return 0;
+        //...END OF "DO NOT CHANGE" AREA
 }
 
 void rearrangeColor (int& Red, int& Green, int& Blue)
 {
-       int tempR, tempG, tempB;
-       
-       tempR = Red;
-       tempG = Green;
-       tempB = Blue;
+        int tempR, tempG, tempB;
 
-       if( Green > Red )
-       {
-       Red = tempG;
-       }
-       if( Blue > Red) 
-       {
-       Red = tempB;
-       }
-       if(( Green < Blue) &&( Red != tempB))
-       {
-       Green = tempB;
-       }
-       if(( Green < Red) && (Red != tempR))
-       {
-       Green = tempR;
-       }
-       if(Blue > tempR)
-       { 
-       Blue = tempR;
-       }
-       if( Blue > tempG)
-       {
-       Blue = tempG;
-       }
-   
+        tempR = Red;
+        tempG = Green;
+        tempB = Blue;
 
-       
+        if( Green > Red )
+        {
+                Red = tempG;
+        }
+        if( Blue > Red) 
+        {
+                Red = tempB;
+        }
+        if(( Green < tempB) && ( Red != tempB))
+        {
+                Green = tempB;
+        }
+        if(( Green < tempR) && (Red != tempR))
+        {
+                Green = tempR;
+        }
+        if(Blue > tempR)
+        { 
+                Blue = tempR;
+        }
+        if( Blue > tempG)
+        {
+                Blue = tempG;
+        }
+
+
+
 
 
 }
